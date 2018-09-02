f7e9db5d196bf885d464bbc60f7510755e5810f8
WMaxZimmerman <william.m.zimmerman@gmail.com>
2018-09-02 13:46:52 -0500

    Added in Scripts for player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public float Speed  { get; set; }
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Speed = 0.1f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    Move();
+    }
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-Speed, 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(Speed, 0));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0, -Speed));
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0, Speed));
+        }
+    }
+}
